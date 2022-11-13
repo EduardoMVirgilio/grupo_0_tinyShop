843b4f288894a0f077377d7c095da35a5f75f171
Eduardo Virgilio <evirgilio@digitalhouse.com>
2022-11-13 17:36:27 -0300

    Second Sprint

---
+++ website/app.js
@@ -0,0 +1,6 @@
+// require native modules like fs and path.
+// install and require npm package.
+// configuration local server
+// configuration utilities for server
+// start up local server
+// setup routes and responses for server
---
+++ website/public/assets/assets.md
@@ -0,0 +1,5 @@
+# Assets
+
+- **_Logo_**
+- **_Catalog Images_**
+- **_Icons and Banners_**
